3bdbdd9eb098fb1fe035cd0f2b4ac860a5888fea
NaikoD <gamergmsns@gmail.com>
2022-12-16 12:27:50 +0700

    Updating

---
+++ Desktop/Portofolio/main/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        main: "#06b6d4"
+      }
+    },
+  },
+  plugins: [],
+}
